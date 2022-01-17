5ccf3a2eee538e5a5cdc469c241c19ceef946a75
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-17 12:19:33 +0200

    Add loading.js component.

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import css from "../index.css";
+
+const Loading = () => {
+    return (
+        <div className={css.loading}>
+            <h1>Loading Tours...</h1>
+        </div>
+    );
+}
+export default Loading;
