2ef398d14ff116260f52bf70a802cdb055238255
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-19 16:24:49 +0200

    Add a new Action type to cartProvider to remove element from cart.

--- src/components/cart/CartProvider.js
+++ src/components/cart/CartProvider.js
@@ -19,6 +19,16 @@ const cartReducer = (state, action) => {
         return { tours: updateTours, amount: updateAmount, image: updateImage };
     }
 
+    if (action.type === "RemoveItem") {
+        const getTourRemoved = state.tours.filter(tour => tour.id === action.id);
+        const newToursCart = state.tours.filter(tour => tour.id !== action.id);
+        const updateAmount = state.amount - 1;
+
+        const newTours = action.tours.concat(getTourRemoved);
+        action.setTours(newTours);
+
+        return { tours: newToursCart, amount: updateAmount }
+    }
     return state;
 };
 
@@ -26,15 +36,16 @@ const CartProvider = props => {
     const {setTour} = props;
     const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-    const removeTourHandler = (tours, tour) => {
-        dispatchCartAction({type: "Remove", setTours: setTour, tours: tours, tour: tour});
-    };
+    const removeTourHandler = (tours, tour) => dispatchCartAction({type: "Remove", setTours: setTour, tours: tours, tour: tour});
+
+    const removeItemHandler = (id, tours) => dispatchCartAction({type: "RemoveItem", setTours: setTour, tours: tours, id: id});
 
     const cartContext = {
         tours: cartState.tours,
         amount: cartState.amount,
         image: cartState.image,
-        removeTour: removeTourHandler
+        removeTour: removeTourHandler,
+        removeItem: removeItemHandler
     };
 
     return(
